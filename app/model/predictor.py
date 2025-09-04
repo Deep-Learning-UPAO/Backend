e28a112b2cc0dab504d165af71204a59341940fa
ErnestoSCL <ernestosaniel123@gmail.com>
2025-09-03 21:43:17 -0500

    Modificación función predict

--- app/model/predictor.py
+++ app/model/predictor.py
@@ -1,3 +1,5 @@
+# pylint: disable=E1101
+
 import tensorflow as tf
 import numpy as np
 from pathlib import Path
@@ -7,19 +9,27 @@ model_path = Path(__file__).resolve().parent.parent.parent / "model.keras"
 model = tf.keras.models.load_model(model_path)
 
 def predecir(df):
+
+    # Asegurarse de que df sea un arreglo de NumPy (si es un DataFrame de pandas)
     input_array = np.array(df).reshape(1, -1)
 
-    # Obtener probabilidad de clase 1
+    # Obtener la probabilidad de clase 1
     prob_clase_1 = model.predict(input_array)[0][0]
     prob_clase_0 = 1 - prob_clase_1
+
+    # Convertir probabilidad a float para evitar problemas de serialización
+    prob_clase_1 = float(prob_clase_1)
+    prob_clase_0 = float(prob_clase_0)
 
     # Aplicar umbral personalizado
     umbral = 0.605
     clase_predicha = int(prob_clase_1 >= umbral)
 
-    # Obtener probabilidad de la clase predicha (como hacía scikit-learn)
+    # Convertir probabilidad a porcentaje (solo la de la clase predicha)
     probabilidad = prob_clase_1 if clase_predicha == 1 else prob_clase_0
     riesgo_autismo = round(probabilidad * 100, 2)
+    # Convertir el riesgo de autismo a float también para evitar problemas
+    riesgo_autismo = float(riesgo_autismo)
 
     return {
         "clase_predicha": clase_predicha,
